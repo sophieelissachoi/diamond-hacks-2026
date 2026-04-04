be6a0ba893b3bd69578d102772dc8d40a8bf8bdb
Sophie Choi <103692454+sophieelissachoi@users.noreply.github.com>
2026-04-04 14:31:04 -0700

    fix: upload

---
+++ frontend/src/pages/Upload.tsx
@@ -0,0 +1,9 @@
+export default function Upload() {
+	return (
+		<div className="upload-page">
+			<h1>Upload Receipt</h1>
+
+			<form>{/* file upload will go here later */}</form>
+		</div>
+	);
+}
